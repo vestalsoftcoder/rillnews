4c9b78a4b9d41855deeda8c794041c6623355748
Don Vitalio <vitaliydonchenko@gmail.com>
2020-10-05 23:18:07 -0400

    Copyright update

--- pages/partials/footer.js
+++ pages/partials/footer.js
@@ -2,10 +2,10 @@ export default () => {
   return (
     <footer className="footer">
       <div style={{ marginTop: 20 }}>
-  
+
       </div>
       <div className="copyright">
-        &copy;&nbsp;&nbsp;{ new Date().getFullYear() } Rillnews Project
+        &copy;&nbsp;{ new Date().getFullYear() } Rillnews Project (Placenta.codes Corp., Toronto, ON, Canada - 1 (647) 549-4025 - Veatl Donq, Originator)
       </div>
     </footer>
   )
